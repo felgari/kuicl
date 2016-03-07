a8e0ed69547ad227c2ac0a798ad999a4b4e412a6
felgari <felgari@gmail.com>
2016-03-07 17:28:23 +0100

    Changed the intial comment of the file.

--- kuicl.py
+++ kuicl.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-"""Script to get a web page.
+"""Calculations with local data and data scrapped from web pages.
 """
 
 import sys
